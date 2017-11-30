a24ae9287262992f8d86c9ad054f5f6c2b30f6ea
Victor Aguirre <victor.aguirre417@gmail.com>
2017-11-30 14:22:35 -0600

    Agregar chart de presidentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TseService } from './tse.service';
+import { HttpModule} from '@angular/http';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    RouterModule.forRoot(routes),
+    HttpModule,
+    ChartsModule,
   ],
-  providers: [],
+  providers: [
+    TseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
